e63cdff76f2772d660e2466ed5a7f6f51835fa0c
r8juliet <philmonaco34@gmail.com>
2017-02-05 15:40:05 -0500

    add logo to wells

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
-var endPoint = "https://wind-bow.gomix.me/twitch-api/streams/";
-user = ["imaqtpie", "asmongold", "towelliee", "sypherpk"];
+var streamEP = "https://wind-bow.gomix.me/twitch-api/streams/";
+var channelEP = "https://wind-bow.gomix.me/twitch-api/channels/";
+user = [
+    "imaqtpie",
+    "asmongold",
+    "towelliee",
+    "sypherpk",
+    "ESL_SC2",
+    "OgamingSC2",
+    "cretetion",
+    "freecodecamp",
+];
+$(document).ready(function() {
+    user.forEach(function(element) {
+        let streamUrl = streamEP + element;
+        let channelUrl = channelEP + element;
+        $.getJSON(streamUrl, function(json) {
+            if (json.stream === null) {
+                $("#offline").append("<p id="+element+" class ='well'>offline</p>");
+            } else {
+                $("#online").append("<p id="+element+" class = 'well'>" + json.stream.channel.display_name + "</p>");
+            }
+        })
 
-user.forEach(function(element) {
-    var url = endPoint + element;
-    $.getJSON(url, function(json) {
-        if (json.stream === null) {
-            $("#apiData").append("offline" +
-                "</br>");
-        } else {
-            $("#apiData").prepend(json.stream.channel.display_name + "</br>");
-        }
-    })
+
+        var image = $.getJSON(channelUrl, function(json) {
+
+          $("#"+element).prepend("<img src="+json.logo+">").delay(1000, 'image')
+        })
+    });
 });
